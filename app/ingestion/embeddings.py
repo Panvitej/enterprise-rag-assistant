805f91f8dc470e861e1747be1e4b9fe88795edc5
Panvitej <phanvitej@gmail.com>
2026-09-22 17:58:26 +0530

    Refactor Embedder class to EmbeddingModel with new methods

--- app/ingestion/embeddings.py
+++ app/ingestion/embeddings.py
@@ -1,13 +1,22 @@
 from sentence_transformers import SentenceTransformer
 
 
-class Embedder:
-    def __init__(self, model_name: str = "BAAI/bge-small-en-v1.5"):
+class EmbeddingModel:
+
+    def __init__(
+        self,
+        model_name: str = "BAAI/bge-small-en-v1.5",
+    ):
         self.model = SentenceTransformer(model_name)
 
-    def encode(self, texts: list[str]):
+    def embed_documents(self, texts: list[str]):
         return self.model.encode(
             texts,
             normalize_embeddings=True,
-    
-        ) 
+        )
+
+    def embed_query(self, query: str):
+        return self.model.encode(
+            query,
+            normalize_embeddings=True,
+        )
